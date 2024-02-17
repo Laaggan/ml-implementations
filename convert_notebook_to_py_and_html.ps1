9468e9b0f79bc6d6100a627cc8e87b712225478d
Linus Lagergren <linus.lagergren@spinit.se>
2024-02-17 18:33:40 +0100

    Changing the directory and using relative path didn't work apparently

--- convert_notebook_to_py_and_html.ps1
+++ convert_notebook_to_py_and_html.ps1
@@ -1,3 +1,2 @@
-Set-Location "C:\Users\llag\source\repos\annat\ml-implementations"
-jupyter nbconvert note.ipynb --to="python" --output-dir="\results" --output="note-convert.py"
-jupyter nbconvert note.ipynb --to="html" --output-dir="\results" --output="note-convert.html"
+jupyter nbconvert note.ipynb --to="python" --output-dir="C:\Users\llag\source\repos\annat\ml-implementations\results" --output="note-convert.py"
+jupyter nbconvert note.ipynb --to="html" --output-dir="C:\Users\llag\source\repos\annat\ml-implementations\results" --output="note-convert.html"
