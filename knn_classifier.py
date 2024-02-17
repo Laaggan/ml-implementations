06ee3be7f8558b6c53922974381c3c3fa4ad7c7b
Linus Lagergren <linus.lagergren@spinit.se>
2024-02-17 18:34:05 +0100

    Add support for minowski distance

--- knn_classifier.py
+++ knn_classifier.py
@@ -1,23 +1,30 @@
 from math import sqrt
 import numpy as np
 
-def euclidean_distance(x1, x2):
-    diff = x1 - x2
-    return sqrt(sum(map(lambda x: x**2, diff)))
+def manhattan_distance(x1, x2):
+    return minowski_distance(x1, x2, 1)
 
-def print_pythagorean_triples_up_to_n(n):
+def euclidean_distance(x1, x2):
+    return minowski_distance(x1, x2, 2)
+
+def minowski_distance(x1, x2, p):
+    diff = x1 - x2
+    return (sum(map(lambda x: x**p, diff)))**(1/p)
+
+def print_pythagorean_triples_up_to_n(n, p=2):
     for i in range(1, n):
         for j in range(i, n):
             x1 = np.array([0, 0])
             x2 = np.array([i, j])
-            result = euclidean_distance(x1, x2)
+            result = minowski_distance(x1, x2, p)
             if not abs(round(result) - result) > 0:
                 print(i, j, result)
 
 
 class KnnClassifier:
-    def __init__(self, K):
+    def __init__(self, K, p=2):
         self.K = K
+        self.p = p
 
     def fit(self, X):
         pass
@@ -25,10 +32,9 @@ class KnnClassifier:
     def predict(self, X, Y, x):
         result = np.zeros(X.shape[0])
         for i, x_i in enumerate(X):
-            result[i] = euclidean_distance(x_i, x)
+            result[i] = minowski_distance(x_i, x, self.p)
         
         sort_index = np.argsort(result)
-
         knn_index = sort_index[:self.K]
         nn = Y[knn_index]
         nn_unique = np.unique(nn)
